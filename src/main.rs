d68a7df28b55fd5d944cecc3ace05ad226f28c5b
rosen1000 <rosen.ruseff@gmail.com>
2022-06-20 18:33:26 +0300

    selecting projects

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
-use inquire::{Confirm, Select, Text};
-use util::{create_sym_link, init_manifest, read_manifest, App, Manifest, ReadManifestError, Meta};
+use inquire::{Confirm, MultiSelect, Select, Text};
+use util::{init_manifest, read_manifest, App, Manifest};
 
 use crate::util::{print, LoggingLevel, META_DATA_VERSION};
 
@@ -10,23 +10,13 @@ pub mod util;
 
 fn main() {
     let manifest = read_manifest();
-    let manifest = match manifest {
-        Ok(m) => {
-            Some(m)
-        }
-        Err(ReadManifestError::NotFound) => {
-            init_manifest();
-            None
-        }
-        Err(ReadManifestError::NoApps) => {
-            Some(Manifest {meta: Meta {version : META_DATA_VERSION}, apps : vec![]})
-        }
-    };
     if manifest.is_none() {
+        init_manifest();
         return;
     }
 
     let manifest = manifest.unwrap();
+    println!("using v{}", manifest.meta.version);
     if manifest.meta.version != META_DATA_VERSION {
         println!("This folder is different version from this program!");
         return;
@@ -45,22 +35,45 @@ fn main() {
 
     match answer {
         Ok(select) => match select {
-            "Open project" => open_project(),
+            "Open project" => open_project(&mut manifest.to_owned()),
             "New project" => new_project(&mut manifest.to_owned()),
-            "Delete project" => delete_project(),
+            "Delete project" => delete_project(&mut manifest.to_owned()),
             "Remove code folder" => delete_code_folder(),
             _ => println!("lmao cringe"),
         },
         Err(_) => println!("lmao cringe"),
     }
-
-    // for app in manifest.app.as_ref().unwrap() {
-    //     println!("{}: {:?}", app.name, app.languages);
-    // }
 }
 
-fn open_project() {
-    println!("open");
+fn open_project(manifest: &mut Manifest) {
+    let select = Select::new("Select mode", vec!["by language", "by tags"]).prompt();
+    match select {
+        Ok(mode) => match mode {
+            "by language" => {
+                let langs_selection =
+                    MultiSelect::new("Select languages", manifest.to_languages()).prompt();
+                if let Ok(langs) = langs_selection {
+                    manifest.apps.iter().for_each(|app| app.dematerialize());
+                    manifest
+                        .search_apps_by_langs(langs)
+                        .iter()
+                        .for_each(|a| a.materialize());
+                }
+            }
+            "by tags" => {
+                let tag_selection = MultiSelect::new("Select tags", manifest.to_tags()).prompt();
+                if let Ok(tags) = tag_selection {
+                    manifest.apps.iter().for_each(|app| app.dematerialize());
+                    manifest
+                        .search_apps_by_tags(tags)
+                        .iter()
+                        .for_each(|a| a.materialize());
+                }
+            }
+            _ => {}
+        },
+        _ => {}
+    }
 }
 
 fn new_project(manifest: &mut Manifest) {
@@ -94,7 +107,7 @@ fn new_project(manifest: &mut Manifest) {
     }
 
     if show {
-        create_sym_link(app.clone().name);
+        app.materialize();
     }
 
     manifest.apps.push(app);
@@ -103,8 +116,28 @@ fn new_project(manifest: &mut Manifest) {
     print("Done!", LoggingLevel::Info);
 }
 
-fn delete_project() {
-    println!("delete");
+fn delete_project(manifest: &mut Manifest) {
+    let select = Select::new(
+        "Which project to delete?",
+        manifest
+            .apps
+            .clone()
+            .iter()
+            .map(|app| app.clone().name)
+            .collect(),
+    )
+    .prompt();
+
+    let name = select.unwrap();
+
+    let app = manifest
+        .apps
+        .swap_remove(manifest.apps.iter().position(|a| a.name == name).unwrap());
+    if let Err(e) = app.nuke() {
+        print(e.to_string(), LoggingLevel::Error);
+    }
+    println!("{:?}", manifest);
+    manifest.save();
 }
 
 fn delete_code_folder() {
--- src/util.rs
+++ src/util.rs
@@ -3,6 +3,7 @@ extern crate serde;
 use inquire::Select;
 use question::{Answer::*, Question};
 use serde_derive::{Deserialize, Serialize};
+use std::collections::HashSet;
 use std::fs::{self, File};
 use std::io::{BufRead, BufReader, Write};
 use toml::toml;
@@ -12,13 +13,7 @@ pub use crate::logger::{print, LoggingLevel};
 const MANIFEST_PATH: &str = "./manifest.toml";
 pub const META_DATA_VERSION: u8 = 1;
 
-#[derive(Debug)]
-pub enum ReadManifestError {
-    NotFound,
-    NoApps
-}
-
-pub fn read_manifest() -> Result<Manifest, ReadManifestError> {
+pub fn read_manifest() -> Option<Manifest> {
     let file = File::open(MANIFEST_PATH);
     let mut manifest_text = String::new();
     match file {
@@ -31,17 +26,24 @@ pub fn read_manifest() -> Result<Manifest, ReadManifestError> {
         }
         Err(_) => {
             print("Manifest.toml not found!", LoggingLevel::Warn);
-            return Err(ReadManifestError::NotFound);
+            // return Err(ReadManifestError::NotFound);
+            return None;
         }
     }
 
     let parsed = toml::from_str(&manifest_text);
     if parsed.is_err() {
-        return Err(ReadManifestError::NoApps);
+        let manifest = Manifest {
+            meta: Meta {
+                version: META_DATA_VERSION,
+            },
+            apps: vec![],
+        };
+        manifest.save();
+        return Some(manifest);
     }
 
-    let parsed = parsed.unwrap();
-    Ok(parsed)
+    Some(parsed.unwrap())
 }
 
 pub fn init_manifest() {
@@ -76,13 +78,17 @@ pub fn init_manifest() {
 }
 
 #[cfg(target_family = "unix")]
-pub fn create_sym_link(app: String) {
-    std::os::unix::fs::symlink(format!(".apps/{}", app), app).unwrap();
+fn create_sym_link(app: String) -> Result<(), std::io::Error> {
+    std::os::unix::fs::symlink(format!(".apps/{}", app), app)
 }
 
 #[cfg(target_family = "windows")]
-pub fn create_sym_link(app: String) {
-    std::os::windows::fs::symlink_dir(format!(".apps/{}", app), app).unwrap();
+fn create_sym_link(app: String) -> Result<(), std::io::Error> {
+    std::os::windows::fs::symlink_dir(format!(".apps/{}", app), app);
+}
+
+fn delete_sym_link(app: String) -> Result<(), std::io::Error> {
+    std::fs::remove_file(app)
 }
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
@@ -103,11 +109,43 @@ pub struct App {
     pub tags: Vec<String>,
 }
 
+impl App {
+    pub fn materialize(&self) {
+        if let Err(e) = create_sym_link(self.clone().name) {
+            if e.raw_os_error().unwrap() != 17 {
+                print(e.to_string(), LoggingLevel::Error);
+            }
+        }
+    }
+
+    pub fn dematerialize(&self) {
+        if let Err(e) = delete_sym_link(self.clone().name) {
+            if e.raw_os_error().unwrap() != 2 {
+                print(e.to_string(), LoggingLevel::Error);
+            }
+        }
+    }
+
+    pub fn nuke(&self) -> Result<(), std::io::Error> {
+        let name = self.clone().name;
+        delete_sym_link(name.clone())?;
+        fs::remove_dir_all(format!(".apps/{}", name))?;
+        Ok(())
+    }
+}
+
 impl Manifest {
     pub fn save(&self) {
         let mut file = File::create(MANIFEST_PATH).unwrap();
-        file.write(toml::to_string(&self).unwrap().as_bytes())
-            .unwrap();
+        let manifest_toml = toml::to_string(&self);
+        println!("{:?}", manifest_toml);
+        if let Ok(to_write) = manifest_toml {
+            if let Err(e) = file.write(to_write.as_bytes()) {
+                print(e.to_string(), LoggingLevel::Error);
+            }
+        } else if let Err(e) = manifest_toml {
+            print(e.to_string(), LoggingLevel::Error);
+        }
     }
 
     pub fn search_app_by_name(&self) -> Option<&App> {
@@ -128,5 +166,57 @@ impl Manifest {
         }
 
         return None;
+    }
+
+    pub fn search_apps_by_tags(&self, tags: Vec<String>) -> Vec<&App> {
+        let mut apps: Vec<&App> = vec![];
+
+        self.apps.iter().for_each(|app| {
+            app.tags.iter().for_each(|tag| {
+                if tags.contains(tag) {
+                    apps.push(app);
+                }
+            })
+        });
+
+        apps
+    }
+
+    pub fn search_apps_by_langs(&self, langs: Vec<String>) -> Vec<&App> {
+        let mut apps: Vec<&App> = vec![];
+
+        self.apps.iter().for_each(|app| {
+            app.languages.iter().for_each(|lang| {
+                if langs.contains(lang) {
+                    apps.push(app);
+                }
+            })
+        });
+
+        apps
+    }
+
+    pub fn to_tags(&self) -> Vec<String> {
+        let mut tag: HashSet<String> = HashSet::new();
+
+        self.apps.clone().iter().for_each(|app| {
+            app.tags.iter().for_each(|t| {
+                tag.insert(t.to_string());
+            });
+        });
+
+        tag.into_iter().collect()
+    }
+
+    pub fn to_languages(&self) -> Vec<String> {
+        let mut langs: HashSet<String> = HashSet::new();
+
+        self.apps.clone().iter().for_each(|app| {
+            app.languages.iter().for_each(|l| {
+                langs.insert(l.to_string());
+            });
+        });
+
+        langs.into_iter().collect()
     }
 }
